2d44bd94e423b4c4a5c6e70fde745d682124717b
Karthik <karthik21@live.in>
2017-12-15 02:32:16 +0530

    api js added

--- web/js/options.js
+++ web/js/options.js
@@ -7,24 +7,31 @@ var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/
 var userId = 'me';
 
 function handleClientLoad() {	
-	gapi.client.setApiKey(API_KEY);
-	window.setTimeout(checkAuth, 1);
+	gapi.load('client:auth2', initClient);
 }
-function checkAuth() {
-	gapi.auth.authorize({
-		client_id: CLIENT_ID,
-		scope: SCOPES,
-		immediate: false
-	}, updateSigninStatus);
-}
+function initClient() {
+    gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    }).then(function () {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
+      // Handle the initial sign-in state.
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.onclick = handleAuthClick;
+      signoutButton.onclick = handleSignoutClick;
+    });
+  }
 
-function updateSigninStatus(authResult) {
-	if(authResult && !authResult.error) {
+function updateSigninStatus(isSignedIn) {
+	if(isSignedIn) {
 		$('#authorize-button').hide();
 		$("#signout-button").show().off('click').on('click',handleSignoutClick);
 		$('#calendarListDiv').show();
-		loadClient();
+		loadCalendars();
 	} else {
 		$('#authorize-button').show();
 		$("#signout-button, #calendarListDiv").hide();
